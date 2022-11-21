fef6c2e3e75349fbcdf942d7d650cfce3ee11f77
Your Name <>
2022-11-22 05:25:46 +0530

    git configurations

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,6 +8,7 @@ async function main() {
 
 
   console.log("tokenSlabs address", TokenSlabs.address);
+  console.log("contract deployed")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
