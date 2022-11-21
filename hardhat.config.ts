7cd6560101ed3181d72b3a32d7d2e4df5384e6af
Your Name <>
2022-11-22 05:16:05 +0530

    configuring .env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: "https://soft-wispy-water.ethereum-goerli.discover.quiknode.pro/978ad9caccfc810fae587d02c27af840fb66a938/",
-      accounts: ["65179b0ee32530f0e97eb9147ce7d0d26e9932f8afc3cefc692785c9e067d2fc"]
+      url: process.env.STAGING_QUIKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
